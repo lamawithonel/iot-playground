d9be01a6d6a3d2cae630b03b3b7553081621cb96
Lucas Yamanishi <lucas.yamanishi@gmail.com>
2026-01-01 14:40:18 -0500

    src/main.rs: Update timer config to use HSE, max speed
    The external (HSE) oscillator is more stable and enables a wider range
    of frequencies at the cost of slightly higher energy consumption.  Since
    I don't care about energy effiency right now, this commit uses it and
    sets the clocks to their max values.

--- feather-stm32f405/src/main.rs
+++ feather-stm32f405/src/main.rs
@@ -25,15 +25,26 @@ mod app {
 
     #[init]
     fn init(_: init::Context) -> (Shared, Local) {
+        // Configure timers to use the HSE clock source and max out frequencies.
         let mut config = embassy_stm32::Config::default();
         {
             use embassy_stm32::rcc::*;
-            config.rcc.hsi = true;
-            config.rcc.sys = Sysclk::HSI;
-            config.rcc.pll = None;
-            config.rcc.ahb_pre = AHBPrescaler::DIV1;
-            config.rcc.apb1_pre = APBPrescaler::DIV1;
-            config.rcc.apb2_pre = APBPrescaler::DIV1;
+            config.rcc.hse = Some(Hse {
+                freq: embassy_stm32::time::Hertz(12_000_000),
+                mode: HseMode::Oscillator,
+            });
+            config.rcc.pll_src = PllSource::HSE;
+            config.rcc.pll = Some(Pll {
+                prediv: PllPreDiv::DIV6,   // 12 MHz / 6 = 2 MHz
+                mul: PllMul::MUL168,       // 2 MHz * 168 = 336 MHz
+                divp: Some(PllPDiv::DIV2), // 336 MHz / 2 = 168 MHz (sysclk)
+                divq: Some(PllQDiv::DIV7), // 336 Mhz / 7 = 48 MHz (USB, SDIO)
+                divr: None,
+            });
+            config.rcc.sys = Sysclk::PLL1_P;
+            config.rcc.ahb_pre = AHBPrescaler::DIV1; // 168 MHz
+            config.rcc.apb1_pre = APBPrescaler::DIV4; // 42 MHz (max for APB1)
+            config.rcc.apb2_pre = APBPrescaler::DIV2; // 84 MHz (max for APB2)
         }
         let p = embassy_stm32::init(config);
         info!("Hello world!");
@@ -41,7 +52,9 @@ mod app {
         let led = Output::new(p.PC1, Level::High, Speed::Low);
         info!("LED initialized on PC1 (red)");
 
-        let timer_clock_hz = 8_000_000;
+        // TIM2 is on APB1.  When APB1 prescaler != 1, timer clock = 2*APB1
+        // APB1 = 42 MHz, so TIM2 clock = 84 MHz
+        let timer_clock_hz = 84_000_000;
         Mono::start(timer_clock_hz);
 
         // Schedule the blinking task
